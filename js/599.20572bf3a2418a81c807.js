8ce488ebfea86c1342a7689f335376938e823243
kirusanov <k.rusanov@riverstart.ru>
2023-12-24 19:06:29 +0400

    booking, product-modal

--- js/599.20572bf3a2418a81c807.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebpack_pug=self.webpackChunkwebpack_pug||[]).push([[599],{2599:(e,u,n)=>{n.r(u),n.d(u,{default:()=>c});var r=n(6252),s={style:{"font-size":"2rem"}};const t={},c=(0,n(3744).Z)(t,[["render",function(e,u){return(0,r.wg)(),(0,r.iD)("div",s,"тестовая страница на vue")}]])}}]);
